affadb1f7579884bf1b6607a57c49dcbe19d5e2a
Eddy Zhou <edward.zhzh@gmail.com>
2020-09-17 18:40:48 -0400

    made some changes

---
+++ CCC Testing Environment.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	cout << "Enter information" << endl;
+	int i = 0;
+	string s = "";
+	double d = 0;
+	cin >> i >> s >> d;
+	cout << i << " " << s << " " << d;
+}
