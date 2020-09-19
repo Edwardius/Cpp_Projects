bef7ac34719bc12149d5f7c41065679e3ba738ac
Eddy Zhou <edward.zhzh@gmail.com>
2020-09-19 16:03:03 -0400

    added arrays and strings notes

---
+++ Cpp Notes/ArraysAndStrings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //there are little to no notes for this section because most is review from java and python
+    //String is a char array
+    string str = "Hello";
+    char str1[] = "Goodbye";
+    cout << str[1]; //both work :)))
+    cout << str1[4];
+    return 0;
+}
